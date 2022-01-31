6b5c7719f9aced35788431737049f9035b16681f
moriaty <19520624@gm.uit.edu.vn>
2022-01-31 19:16:40 +0700

    refactor tfidf

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
 import os
-import unidecode
 
 import pandas as pd
-import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
 
+from tfidf import TFIDF
 from config import *
 
 # Load corpus and query
 df_corpus = pd.read_csv(os.path.join(DATASET_DIR, 'corpus.csv'))
 df_query = pd.read_csv(os.path.join(DATASET_DIR, 'query.csv'))
 
-# Create Td-idf model & embed documents
-model_text = TfidfVectorizer(stop_words='english', binary=True, max_features=25_000)
-embeddings_corpus = model_text.fit_transform(df_corpus.title).toarray()
+# Create TF-IDF model
+tfidf = TFIDF(df_corpus)
 
-# Query embedding
-query = df_query.iloc[:]
-embeddings_query = model_text.transform(query.title).toarray()
-
-# Matrix multiplication to compute cosine similarity
-cosine_similarity = np.transpose(embeddings_corpus @ embeddings_query.T)
-
-# Output results
-results = []
-for group in cosine_similarity:
-    matched_index = np.where(group >= TFIDF_THRESHOLD)
-    matched_list = df_corpus.iloc[matched_index[0]]['posting_id'].to_list()
-    results.append(matched_list)
+results = tfidf.query(df_query)
 print(results)
---
+++ tfidf.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from config import *
+
+class TFIDF:
+
+    def __init__(self, df_corpus):
+        self.df_corpus = df_corpus
+        # Create Td-idf model & embed documents
+        self.model_text = TfidfVectorizer(stop_words='english', binary=True, max_features=25_000)
+        self.embeddings_corpus = self.model_text.fit_transform(df_corpus.title).toarray()
+
+    def query(self, df_query):
+        # Query embedding
+        embeddings_query = self.model_text.transform(df_query.title).toarray()
+
+        # Matrix multiplication to compute cosine similarity
+        cosine_similarity = np.transpose(self.embeddings_corpus @ embeddings_query.T)
+
+        # Output results
+        results = {}
+        for (_, query), group in zip(df_query.iterrows(), cosine_similarity):
+            matched_index = np.where(group >= TFIDF_THRESHOLD)
+            matched_list = self.df_corpus.iloc[matched_index[0]].posting_id.to_list()
+            results[query.posting_id] = matched_list
+
+        return results
