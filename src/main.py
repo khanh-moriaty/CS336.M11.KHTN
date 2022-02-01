efa65f671a62837fed27c1fc6b44c719008f5a0c
moriaty <19520624@gm.uit.edu.vn>
2022-02-01 10:10:38 +0700

    refactor dir structure

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 
 from tfidf import TFIDF
+from bm25 import BM25
 from evaluate import EvaluateMAP
 from config import *
 
@@ -10,11 +11,16 @@ from config import *
 df_corpus = pd.read_csv(os.path.join(DATASET_DIR, 'corpus.csv'))
 df_query = pd.read_csv(os.path.join(DATASET_DIR, 'query.csv'))
 
+evaluate_map = EvaluateMAP(df_corpus, df_query)
+
+# bm25 = BM25(df_corpus)
+# pred = bm25.query(df_query)
+# print(evaluate_map(pred))
+
 for threshold in range(1, 10):
     # Create TF-IDF model
     tfidf = TFIDF(df_corpus, threshold=threshold/10)
 
-    results = tfidf.query(df_query)
+    pred = tfidf.query(df_query)
 
-    evaluate_map = EvaluateMAP(df_corpus, df_query)
-    print(threshold/10, evaluate_map(results))
+    print(threshold/10, evaluate_map(pred))
