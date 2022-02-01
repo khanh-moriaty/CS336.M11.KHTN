10f222ca8ae8d5e02c336bf81313f79c7a3e8a8a
moriaty <19520624@gm.uit.edu.vn>
2022-02-01 12:27:56 +0700

    refactor main.py

--- src/main.py
+++ src/main.py
@@ -7,20 +7,21 @@ from bm25 import BM25
 from evaluate import EvaluateMAP
 from config import *
 
-# Load corpus and query
-df_corpus = pd.read_csv(os.path.join(DATASET_DIR, 'corpus.csv'))
-df_query = pd.read_csv(os.path.join(DATASET_DIR, 'query.csv'))
+if __name__ == '__main__':
+    # Load corpus and query
+    df_corpus = pd.read_csv(os.path.join(DATASET_DIR, 'corpus.csv'))
+    df_query = pd.read_csv(os.path.join(DATASET_DIR, 'query.csv'))
 
-evaluate_map = EvaluateMAP(df_corpus, df_query)
+    evaluate_map = EvaluateMAP(df_corpus, df_query)
 
-# bm25 = BM25(df_corpus)
-# pred = bm25.query(df_query)
-# print(evaluate_map(pred))
+    # bm25 = BM25(df_corpus)
+    # pred = bm25.query(df_query)
+    # print(evaluate_map(pred))
 
-for threshold in range(1, 10):
-    # Create TF-IDF model
-    tfidf = TFIDF(df_corpus, threshold=threshold/10)
+    for threshold in range(1, 10):
+        # Create TF-IDF model
+        tfidf = TFIDF(df_corpus, threshold=threshold/10)
 
-    pred = tfidf.query(df_query)
+        pred = tfidf.query(df_query)
 
-    print(threshold/10, evaluate_map(pred))
+        print(threshold/10, evaluate_map(pred))
