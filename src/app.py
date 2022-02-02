c896da5474dfaf8c7c534a79079a92f202aad5d1
moriaty <19520624@gm.uit.edu.vn>
2022-02-02 12:42:57 +0000

    add experiment request

--- src/app.py
+++ src/app.py
@@ -7,9 +7,45 @@ from flask import Flask, request
 from models.text.tfidf import TFIDF
 from models.text.bm25 import BM25
 from utils.evaluate import EvaluateMAP
-from config import *
+import config
+
+# Load corpus and query
+df_corpus = pd.read_csv(os.path.join(config.DATASET_DIR, 'corpus.csv'))
+df_query = pd.read_csv(os.path.join(config.DATASET_DIR, 'query.csv'))
+
+models = {
+    'tfidf': TFIDF(df_corpus, config.TFIDF_THRESHOLD)
+}
 
 app = Flask(__name__)
+
+def query_df(df_query, model_text, model_image):
+    start_time = time.time()
+
+    if model_text in ['tfidf', 'bm25']:
+        results = models[model_text](df_query)
+
+    if model_image in ['sift']:
+        pass
+
+    end_time = time.time()
+    processing_time = end_time - start_time
+
+    return results, processing_time
+
+@app.route("/v1/exp")
+def query_exp():
+    model_text = request.args.get('model_text', default='')
+    model_image = request.args.get('model_image', default='')
+
+    results, processing_time = query_df(df_query, model_text, model_image)
+
+    return {
+        'model_text': model_text,
+        'model_image': model_image,
+        'processing_time': processing_time,
+        'results': results,
+    }
 
 @app.route("/v1/query")
 def query():
@@ -17,24 +53,22 @@ def query():
     image = request.args.get('image', default='')
     model_text = request.args.get('model_text', default='')
     model_image = request.args.get('model_image', default='')
-    response = {
+
+    df_query = pd.DataFrame(data={
+        'title': [text], 
+        'image': [image], 
+        'posting_id': [''], 
+        'image_phash': [''], 
+        'label_group': [0]
+    })
+
+    results, processing_time = query_df(df_query, model_text, model_image)
+
+    return {
         'text': text,
         'image': image,
         'model_text': model_text,
         'model_image': model_image,
-        'processing_time': 0,
-        'results': {}
+        'processing_time': processing_time,
+        'results': results[''],
     }
-
-    start_time = time.time()
-
-    if text and model_text in ['tfidf', 'bm25']:
-        pass
-
-    if image and model_image in ['sift']:
-        pass
-
-    end_time = time.time()
-    response['processing_time'] = end_time - start_time
-
-    return response
